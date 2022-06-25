54bae42227d39ed4e09185864afa1f3803604846
Saullo Bretas <saullo.bretas55@gmail.com>
2022-06-24 21:51:37 -0300

    System: Mount all filesystems on startup

--- Services/System/Main.cpp
+++ Services/System/Main.cpp
@@ -1,3 +1,33 @@
-int main() {
-    return 0;
+#include <cstdio>
+#include <cstdlib>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void system_mount_all_fs()
+{
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        std::perror("fork()");
+        std::exit(pid);
+    }
+    else if (!pid)
+    {
+        auto result = execl("/bin/mount", "mount", "-a", nullptr);
+        if (result < 0)
+        {
+            std::perror("execl()");
+            std::exit(result);
+        }
+    }
+    else
+        wait(nullptr);
+}
+
+int main()
+{
+    system_mount_all_fs();
+
+    while (true)
+        ;
 }
