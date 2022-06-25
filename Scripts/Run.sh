0b6fb4b2db9980e3a51170b512733f998ab7f408
Saullo Bretas <saullo.bretas55@gmail.com>
2022-06-24 22:04:14 -0300

    Scripts: Renamed Run.sh variables

--- Scripts/Run.sh
+++ Scripts/Run.sh
@@ -1,16 +1,16 @@
-[ -z "$NASHIOS_QEMU_BIN" ] && NASHIOS_QEMU_BIN="qemu-system-i386"
-[ -z "$NASHIOS_QEMU_KERNEL" ] && NASHIOS_QEMU_KERNEL="Sysroot/boot/bzImage"
-[ -z "$NASHIOS_QEMU_IMAGE" ] && NASHIOS_QEMU_IMAGE="QEMU.img"
-[ -z "$NASHIOS_QEMU_ARGS" ] && NASHIOS_QEMU_ARGS="
-    -kernel $NASHIOS_QEMU_KERNEL
+[ -z "$MABLEOS_QEMU_BIN" ] && MABLEOS_QEMU_BIN="qemu-system-i386"
+[ -z "$MABLEOS_QEMU_KERNEL" ] && MABLEOS_QEMU_KERNEL="Sysroot/boot/bzImage"
+[ -z "$MABLEOS_QEMU_IMAGE" ] && MABLEOS_QEMU_IMAGE="QEMU.img"
+[ -z "$MABLEOS_QEMU_ARGS" ] && MABLEOS_QEMU_ARGS="
+    -kernel $MABLEOS_QEMU_KERNEL
     -serial stdio
-    -drive file=$NASHIOS_QEMU_IMAGE,index=0,media=disk,format=raw"
-[ -z "$NASHIOS_QEMU_APP_ARGS" ] && NASHIOS_QEMU_APP_ARGS="
+    -drive file=$MABLEOS_QEMU_IMAGE,index=0,media=disk,format=raw"
+[ -z "$MABLEOS_QEMU_APP_ARGS" ] && MABLEOS_QEMU_APP_ARGS="
     root=/dev/sda
     console=ttyS0
     init=/bin/SystemService"
 
-[ -e /dev/kvm ] && [ -r /dev/kvm ] && [ -w /dev/kvm ] && NASHIOS_QEMU_VIRT="-enable-kvm"
+[ -e /dev/kvm ] && [ -r /dev/kvm ] && [ -w /dev/kvm ] && MABLEOS_QEMU_VIRT="-enable-kvm"
 
 # shellcheck disable=SC2086
-$NASHIOS_QEMU_BIN --append "$NASHIOS_QEMU_APP_ARGS" $NASHIOS_QEMU_ARGS $NASHIOS_QEMU_VIRT
+$MABLEOS_QEMU_BIN --append "$MABLEOS_QEMU_APP_ARGS" $MABLEOS_QEMU_ARGS $MABLEOS_QEMU_VIRT
